723a77d14410de3fb2912963256f0c0661414b54
devasuryapradeep <devasurya.pradeep@theelixrlabs.com>
2024-01-11 09:23:03 +0530

    Improvement in the Login Screen.

--- Login/LoginViewController.swift
+++ Login/LoginViewController.swift
@@ -8,18 +8,43 @@
 import UIKit
 
 class LoginViewController: UIViewController {
-
+    
+    /// Variable declaration
+    var stringOne = "Don't have an account? Sign up"
+    var stringTwo = "Sign up"
+    
+    /// Refercing  Outlets
+    @IBOutlet weak var outerView: UIView!
+    @IBOutlet weak var signInPromptLabel: UILabel!
     @IBOutlet weak var credentialView: UIImageView!
+    
+    /// View Life cycle.
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        UISetup()
     }
-
-    func UIUpdate(){
-        credentialView.layer.shadowColor = UIColor.black.cgColor
-        credentialView.layer.shadowOpacity = 1
-          credentialView.layer.shadowOffset = CGSize.zero
-        
+    
+    
+    /// Function to get the basic layout.
+    func UISetup(){
+        credentialView.layer.cornerRadius = 20
+        credentialView.clipsToBounds = true
+       
+        outerView.layer.cornerRadius = 20
+        outerView.layer.shadowColor = UIColor.gray.cgColor
+        outerView.layer.shadowOffset = CGSize(width: 5.0, height: 5.0)
+        outerView.layer.shadowRadius = 20
+        outerView.layer.shadowOpacity = 0.7
     }
+    
+    let myLabel = UILabel()
+    var strOne = "Swift 3 has come"
+    let strTwo = "has"
+    let label = UILabel()
+    let stringValue = "There are 5 results."
+    let attributedString: NSMutableAttributedString = NSMutableAttributedString(string: stringValue)
+    attributedString.setColor(color: UIColor.red, forText: "5")
+    label.font = UIFont.systemFont(ofSize: 26)
+    label.attributedText = attributedString
+    self.view.addSubview(label)
 }
-
