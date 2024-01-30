4dea75b817d86cbf1eac4286c99ea764374ce86b
devasuryapradeep <devasurya.pradeep@theelixrlabs.com>
2024-01-30 09:51:30 +0530

    Updates in the homeView

--- ElixrTalentConnect/Helper/MainTabController.swift
+++ ElixrTalentConnect/Helper/MainTabController.swift
@@ -11,18 +11,28 @@ class MainTabController: UITabBarController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        changeRadiusOfTabBar()
+        changeUnSelectedColor()
+        ChangeHeightOfTabbar()
         self.navigationItem.setHidesBackButton(true, animated: true)
     }
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func changeRadiusOfTabBar(){
+        self.tabBar.layer.masksToBounds = true
+             self.tabBar.isTranslucent = true
+             self.tabBar.layer.cornerRadius = 30
+             self.tabBar.layer.maskedCorners = [.layerMinXMinYCorner, .layerMaxXMinYCorner]
     }
-    */
-
+    func changeUnSelectedColor(){
+        self.tabBar.unselectedItemTintColor = UIColor.white
+           }
+    func ChangeHeightOfTabbar(){
+      
+          if UIDevice().userInterfaceIdiom == .phone {
+              var tabFrame            = tabBar.frame
+              tabFrame.size.height    = 100
+              tabFrame.origin.y       = view.frame.size.height - 100
+              tabBar.frame            = tabFrame
+          }
+      
+         }
 }
